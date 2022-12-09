df013a1c57bb6b741a75e42c163706696e48b821
kobygrah <kobygrah24@gmail.comssh-keygen -t rsa -b 4096 -C kobygrah24@gmail.comcd ~/.sshlearclearcd ~/>
2022-12-09 13:33:09 -0800

    Initial Commit

---
+++ Markup Manager/Markup_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Markup_ManagerApp.swift
+//  Markup Manager
+//
+//  Created by Koby Grah on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Markup_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
