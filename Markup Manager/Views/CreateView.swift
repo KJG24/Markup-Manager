562a03056bef555aa55a255e901bbd42b57dedb1
KJG24 <kobygrah24@gmail.com>
2022-12-12 15:29:24 -0800

    Still working on getting feature for users to upload a PDF

--- Markup Manager/Views/CreateView.swift
+++ Markup Manager/Views/CreateView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct CreateView: View {
+    func importPDF() {
+        let documentPicker = UIDocumentPickerViewController(documentTypes: kUTTypePDF, in: .import)
+        documentPicker.delegate = self
+        documentPicker.allowsMultipleSelection = true
+        documentPicker.directoryURL = "/Users"
+        present(documentPicker, animated: true, completion: nil)
+    }
     var body: some View {
         NavigationStack {
             List {
---
+++ Markup Manager/Views/GetFileView.swift
@@ -0,0 +1,21 @@
+//
+//  GetFile.swift
+//  Markup Manager
+//
+//  Created by Koby Grah on 12/12/22.
+//
+
+import SwiftUI
+
+
+struct GetFile: View {
+        var body: some View {
+        importPDF()
+    }
+}
+
+struct GetFile_Previews: PreviewProvider {
+    static var previews: some View {
+        GetFile()
+    }
+}
