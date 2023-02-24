a5f714f811b95769bfd6e2a356af5dea9b13bdf3
KJG24 <kobygrah24@gmail.com>
2023-02-24 10:43:11 -0800

    Added functionality to the ProjectView file to display all saved projects

--- Markup Manager/Views/ProjectView.swift
+++ Markup Manager/Views/ProjectView.swift
@@ -8,10 +8,31 @@
 import SwiftUI
 
 struct ProjectView: View {
+    let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+
     var body: some View {
-        Text("Pick a project")
+        List {
+            ForEach(getDirectories(), id: \.self) { directoryURL in
+                Text(directoryURL.lastPathComponent)
+            }
+        }
+    }
+
+    func getDirectories() -> [URL] {
+        do {
+            let contents = try FileManager.default.contentsOfDirectory(at: documentsDirectoryURL, includingPropertiesForKeys: nil, options: [.skipsHiddenFiles, .skipsSubdirectoryDescendants])
+            return contents.filter { (url) -> Bool in
+                var isDirectory: ObjCBool = false
+                FileManager.default.fileExists(atPath: url.path, isDirectory: &isDirectory)
+                return isDirectory.boolValue
+            }
+        } catch {
+            print("Error getting directories: \(error.localizedDescription)")
+            return []
+        }
     }
 }
+
 
 struct ProjectView_Previews: PreviewProvider {
     static var previews: some View {
